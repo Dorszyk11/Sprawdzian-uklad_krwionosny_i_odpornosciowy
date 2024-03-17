90d2b5133e2d1e5438d4c8bfec4bbd585bd6b419
Dorszyk11 <tymbeixpoi@gmail.com>
2024-03-17 12:51:02 +0100

    gotowa sciaga

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css';
+
+const Header = () => {
+    return(
+        <div className="container header">
+            <h1>Ściąga na <span>| Biologię</span></h1>
+            <p>Sprawdzian - układ krwionośny i odpornościowy</p>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/db/topics.js
@@ -0,0 +1,48 @@
+export const Topics = [
+  {
+    id: 1,
+    temat: `Układ krionośny`,
+    opis: `Funkcje krwi:
+    Funkcja regulacyjna - polaga ona na utrzamaniu ciepła, pH
+    Funkcja ochronna, obronna i odpornościowa
+    Kobieta - 4,5M - 5M w mililitrze 
+    Mężczyzna 
+    
+    hemofilia - choroba genetyczna polega na niedoborze lub całkowitym braku jednego z białek krwi niezbędnego w procesie krzepnięcia
+    
+    Tętnice wyprowadzają krew z serca.
+    Najważniejsza tętnica to Aorta. Jest największa i najgrubsza.
+    Tętnicami płynie krew utlenowana`,  
+    daty: ``,
+  },
+  {
+    id: 2,
+    temat: `Zaburzenia i choroby układu addechowego`,
+    opis: `Zburzona albo nadmierna odporność monologiczna.
+    Zespół nabytrgo niedoboru odporności.
+    HIV niszczy limfocyty T
+    Alergia to najmierna reakcja immunologiczna i jest wynikiem nadwrażliwości układu odpornościowego, który reaguje na antygeny nie-szkodliwe dla normalnego organizmu
+    Choroby atoimmunologiczne - kiedy organizm zaczyna zwalczać własne komurki(reumatoidalne zapalenie stawów, stwardnienie rozsiane, łysienie plackowate)
+    wstrząs immunologiczne - gwałtowna reakcja odpornościowa, która występuje w całym organizmie np na urządlenie przszczoły. objawia się gwałtowym obrzękiem drug oddechowych
+    Transplantacja - przeszczep narządu, często jedyna opcja uratowania życie. Można przeszczepić (płuca, rogówkę oka, serce, wątrobę, nerki, szpik kostny, kości, naczynia krwionośne, skóre). Niektóre narządy są pobierane od osób już nie żyjących a niektóre są pobierane od osób żyjących.
+    Zgodność tkankowa - kiedy antygeny HLA się pogrywają to jest duża szansa, że przeszczep się uda.
+    Konflikt serologiczny - to konflikt pomięczny matką i rozwijającym się płodem.`,
+    daty: ``,
+    },
+  {
+    id: 3,
+    temat: `grupy krwi`,
+    opis: `
+    A   ----->   anty B
+    B   ----->   anty A
+    AB  ----->   nic
+    0   ----->   anty AB    `,
+    daty: ``,
+  },
+  {
+    id: 4,
+    temat: ``,
+    opis: ``,
+    daty: ``,
+  },
+]
